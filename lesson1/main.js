f51bae5830f72ea243d8202b6c44e4d9f8e22845
Yuriipro100 <buyrem06@gmail.com>
2022-11-02 18:41:09 +0200

    add lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,43 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+    // Вивести кожну змінну за допомогою: console.log
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true
+let a = 'hello';
+console.log(a)
+let b = 'owu';
+console.log(b);
+let c = 'com';
+console.log(c);
+let d = 'ua';
+console.log(d);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+const P= 3.14
+console.log(P);
+let s= 2.7;
+console.log(s);
+let v=16;
+console.log(16);
+
+let s1 = 'Ivan';
+let s2 = 'Ivanovich';
+let s3 = 'Ivanov';
+let result = `${s1} ${s2} ${s3}`;
+console.log(result);
+
+let a1 = 100 ;
+console.log(typeof a1);
+let b2 = '100';
+console.log(typeof b2);
+let c2 = true;
+console.log(typeof c2);
+$
